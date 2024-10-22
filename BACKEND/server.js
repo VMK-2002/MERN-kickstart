bb21e62215756a99ad406e88865d645b0e5567e6
VMK <vmksince2002@gmail.com>
2024-10-22 20:30:11 +0530

    changes in server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 import {connectDB} from "./config/db.js";
 import productRoute from './Router/productRoute.js';
-//import cors from "cors";
+import cors from "cors";
 import path from "path";
 
 
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
@@ -21,7 +21,7 @@ app.use("/api/products", productRoute);
 //     allowedHeaders: ['Content-Type'],
 //   };
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 if(process.env.NODE_ENV === "production"){
